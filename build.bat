85689fb7eae65556e6a24a14af900ae30572a923
kanzakirei <kanzakirei.aqua@gmail.com>
2024-11-22 00:02:53 +0900

    テクスチャの更新

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 chcp 65001
 set FILE_NAME=CustomModelData
 
-mkdir %FILE_NAME%
+rem mkdir %FILE_NAME%
 
-xcopy /E /Y WeaponMechanicsResourcePack %FILE_NAME%
-xcopy /E /Y KRSRV_CustomModelData %FILE_NAME%
+rem xcopy /E /Y WeaponMechanicsResourcePack %FILE_NAME%
+rem xcopy /E /Y KRSRV_CustomModelData %FILE_NAME%
 
 rem powershell compress-archive -Path '%FILE_NAME%/*' -DestinationPath '%FILE_NAME%.zip' -Force
 
